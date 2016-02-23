3b0fee054ec8c54be2c985100ebe8804463d4849
SilverFox70 <wfbrinkert@gmail.com>
2016-02-22 19:58:36 -0800

    Unity game tutorial I completed using C#

---
+++ Assets/DisappearMe.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisappearMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
